6f7bae096ee25cfb7c2e5e5c079f2443fada9ab5
lizhuang <lizhuang01@kuyumall.com>
2018-05-28 15:47:24 +0800

    update

--- http-proxy-middleware/server.js
+++ http-proxy-middleware/server.js
@@ -3,7 +3,7 @@ var proxy = require('http-proxy-middleware');
 
 var app = express();
 
-app.use('', proxy({
+app.use('/', proxy({
   target: 'http://120.79.35.65',
   changeOrigin: true,
   // pathRewrite: {
